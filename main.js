7320e801a180a0610b8d485db939665d5ccd06af
Elias <afloresp3@unemi.edu.ec>
2024-08-22 12:20:34 -0500

    Arreglando Dependencias de wsp

--- main.js
+++ main.js
@@ -6,6 +6,19 @@ const fa = require('./funcionesAlertas');
 const wsp = require('./wspAPi'); // Asegúrate de que el nombre del archivo coincida
 require('dotenv').config();
 
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    headless: true
+  });
+  const page = await browser.newPage();
+  await page.goto('https://example.com');
+  await browser.close();
+})();
+
+
 
 // Zona horaria de Ecuador
 const ecuadorTz = 'America/Guayaquil';
